c835bcc263e1bc0cbe2abfdfb000b20a8763205e
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2022-12-03 10:35:01 +0100

    Clean code

--- PP Project 1/main.cpp
+++ PP Project 1/main.cpp
@@ -6,50 +6,58 @@
    to eliminate from your program numerical values by replacing them
    with well defined identifiers */
 #define BOARD_SIZE 13
-#define BOARD_BEGGINING_X 1
-#define BOARD_BEGGINING_Y 3
+#define BOARD_BEGGINING_X 50
+#define BOARD_BEGGINING_Y 1
 #define BOARD_GAP_SIZE_X 4
 #define BOARD_GAP_SIZE_Y 2
+#define BOARD_SIZE_LIMIT 10
 
-#define TEXT_COLOR LIGHTMAGENTA
+#define TEXT_COLOR BLUE
 #define BACKGROUND_COLOR DARKGRAY
 
 #define ARROW_DOWN 0x48
 #define ARROW_UP 0x50
 #define ARROW_LEFT 0x4b
 #define ARROW_RIGHT 0x4d
-#define CURSOR '0'
+#define CURSOR 'O'
 
-void draw_board(int start_x, int start_y, int size) {
-	textcolor(TEXT_COLOR);
-	for (int y = start_y; y <= size * BOARD_GAP_SIZE_Y + start_y; y++)
-	{
-		if ((y-start_y+1) % BOARD_GAP_SIZE_Y) {
-			for (int x = start_x; x <= size * BOARD_GAP_SIZE_X + start_x; x++) {
-				gotoxy(x, y);
-				if ((x - start_x) % BOARD_GAP_SIZE_X == 0) {
-					putch('+');
-				}
-				else {
-					putch('-');
-				}
-			}
-		}
-		else {
-			for (int x = start_x; x <= size * BOARD_GAP_SIZE_X + start_x; x++) {
-				gotoxy(x, y);
-				if ((x - start_x) % BOARD_GAP_SIZE_X == 0) {
-					putch('|');
-				}
-			}
-		}
-	}
-}
+typedef struct coordinates {
+	int x;
+	int y;
+} coordinates_t;
 
-void draw_info(int start_x, int start_y) {
+typedef struct board {
+	char **fields;
+	int size;
+} board_t;
+
+typedef struct game {
+	coordinates_t *cursor_position;
+	board_t *board;
+} game_t;
+
+void draw_info(coordinates_t start_coordinates) {
 	textcolor(TEXT_COLOR);
-	gotoxy(start_x, start_y);
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("Jerzy, Szyjut, 193064");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("arrows  = cursor movement");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
 	cputs("q       = exit");
+	gotoxy(start_coordinates.x, start_coordinates.y++);	
+	cputs("n       = start new game");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("enter   = confirm choice and end player's turn");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("esc     = cancel current action");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("i       = place a stone on the board");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("s       = save the game state");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("l       = load the game state");
+	gotoxy(start_coordinates.x, start_coordinates.y++);
+	cputs("f       = finish the game");
 }
 
 void clear_screen() {
@@ -57,63 +65,100 @@ void clear_screen() {
 	clrscr();
 }
 
-void draw_cursor(int x, int y, int color) {
-	gotoxy(x, y);
+coordinates_t* get_cursor_position_from_board_position(coordinates_t cursor_board_position) {
+	coordinates_t* cursor_screen_position = new coordinates_t;
+	cursor_screen_position->x = BOARD_GAP_SIZE_X * cursor_board_position.x + BOARD_BEGGINING_X;
+	cursor_screen_position->y = BOARD_GAP_SIZE_Y * cursor_board_position.y + BOARD_BEGGINING_Y;
+	return cursor_screen_position;
+}
+
+void draw_cursor(coordinates_t board_cursor_position, int color) {
+	coordinates_t *cursor_posiiton = get_cursor_position_from_board_position(board_cursor_position);
+	gotoxy(cursor_posiiton->x, cursor_posiiton->y);
 	textcolor(color);
 	textbackground(BACKGROUND_COLOR);
 	putch(CURSOR);
+	delete cursor_posiiton;
+}
+
+void handle_movement(coordinates_t* cursor_board_position) {
+	int zn;
+	zn = getch();
+	if (zn == ARROW_DOWN)
+	{
+		if (cursor_board_position->y > 0) {
+			cursor_board_position->y--;
+		}
+	}
+	else if (zn == ARROW_UP)
+	{
+		if (cursor_board_position->y < BOARD_SIZE) {
+			cursor_board_position->y++;
+		}
+	}
+	else if (zn == ARROW_LEFT)
+	{
+		if (cursor_board_position->x > 0) {
+			cursor_board_position->x--;
+		}
+	}
+	else if (zn == ARROW_RIGHT) {
+		if (cursor_board_position->x < BOARD_SIZE) {
+			cursor_board_position->x++;
+		}
+	};
+}
+
+board_t *initialize_board(int size) {
+	board_t *board = new board_t;
+
+	char** fields = new char*[size];
+	for (int i = 0; i < size; i++) {
+		fields[i] = new char[size];
+	}
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			fields[i][j] = 0;
+		}
+	}
+
+	board->fields = fields;
+	board->size = size;
+
+	return board;
+}
+
+game_t* initialize_game() {
+	game_t *game = new game_t;
+	game->cursor_position = new coordinates_t;
+	game->cursor_position->x = 0;
+	game->cursor_position->y = 0;
+	game->board = initialize_board(BOARD_SIZE);
+	return game;
 }
 
 int main() {
-	int zn = 0, x = BOARD_BEGGINING_X, y = BOARD_BEGGINING_Y, current_player = 0;
-	char txt[32];
+	int zn = 0;
+	coordinates_t cursor_board_position = { 0, 0 };
+	coordinates_t info_start_coordinates = { 1, 1 };
+
 #ifndef __cplusplus
 	Conio2_Init();
 #endif
+
 	settitle("Jerzy, Szyjut, 193064");
 	_setcursortype(_NOCURSOR);
 	do {
-		int player_color;
-		if (current_player % 2 == 0) {
-			player_color = WHITE;
-		}
-		else {
-			player_color = BLACK;
-		}
 		clear_screen();
-		draw_info(1, 1);
-		draw_board(BOARD_BEGGINING_X, BOARD_BEGGINING_Y, BOARD_SIZE);
-		draw_cursor(x, y, player_color);
-		current_player++;
-		current_player = current_player % 2;
 		
+		draw_info(info_start_coordinates);
 
+		draw_cursor(cursor_board_position, WHITE);
+		
 		zn = getch();
 		if (zn == 0) {
-			zn = getch();
-			if (zn == ARROW_DOWN)
-			{
-				if (y > BOARD_BEGGINING_Y) {
-					y-=BOARD_GAP_SIZE_Y;
-				}
-			}
-			else if (zn == ARROW_UP)
-			{
-				if (y < BOARD_SIZE * BOARD_GAP_SIZE_Y + BOARD_BEGGINING_Y) {
-					y+=BOARD_GAP_SIZE_Y;
-				}
-			}
-			else if (zn == ARROW_LEFT)
-			{
-				if (x > BOARD_BEGGINING_X) {
-					x-=BOARD_GAP_SIZE_X;
-				}
-			}
-			else if (zn == ARROW_RIGHT) {
-				if (x < BOARD_SIZE * BOARD_GAP_SIZE_X + BOARD_BEGGINING_X) {
-					x+=BOARD_GAP_SIZE_X;
-				}
-			};
+			handle_movement(&cursor_board_position);
 		}
 	} while (zn != 'q');
 
